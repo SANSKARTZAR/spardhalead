6f24856c9b5939087154d7e771d314b9b7e48d63
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-08-09 14:15:15 +0530

    newicon

--- src/app/component/spardhalead/CourseCertifications.js
+++ src/app/component/spardhalead/CourseCertifications.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import certification from "@/app/assets/images/best-online-guitar-lessons.png";
-
+import support from "@/app/assets/images/icons/support.png";
+import guitar from "@/app/assets/images/icons/guitar.png";
+import doc from "@/app/assets/images/icons/documents.png";
 
 
 
@@ -21,7 +23,7 @@ export default function CourseCertifications() {
                         <div className="col-lg-6 col-md-6 col-12">
                         <div className="certificatecards-div">
                             <div className="certificatecards">
-                                <Image src={certification.src} width="90" height="90" alt="certificate-img" />
+                                <Image src={doc.src} width="90" height="90" alt="certificate-img" />
                                 <span>
                                     <h1 className="certif-title">Goal Based Guitar Lessons</h1>
                                     <p className="certif-para">You will learn from a qualified Guitar teacher, who has already done it all. Grade exams, stage shows and performances. Set your goals, then the Guitar teacher will help you with right lesson plan, exercises and track progress to get faster results.</p>
@@ -29,7 +31,7 @@ export default function CourseCertifications() {
                             </div>
                             <br/>
                             <div className="certificatecards">
-                                <Image src={certification.src} width="90" height="90" alt="certificate-img" />
+                                <Image src={guitar.src} width="90" height="90" alt="certificate-img" />
                                 <span>
                                      <h1 className="certif-title">Play Your Favourite Songs</h1>
                                      <p className="certif-para">The whole point is to play your favourite songs in Guitar. Discuss your favourite songs with your Guitar teacher & your tutor will help you learn quickly. All you have to do is to focus on practise.</p>
@@ -37,7 +39,7 @@ export default function CourseCertifications() {
                             </div>
                             <br/>
                             <div className="certificatecards">
-                                <Image src={certification.src} width="90" height="90" alt="certificate-img" />
+                                <Image src={support.src} width="90" height="90" alt="certificate-img" />
                                 <span>
                                      <h1 className="certif-title">Friendly Online Guitar Teacher</h1>
                                      <p className="certif-para">Your Guitar teacher will keep you motivated and you can reach them to get your doubts cleared. You can send recorded videos and ask for feedback. The teacher will help you to achieve your goal and will support in your Guitar journey.</p>
