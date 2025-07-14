7ab76cd8009ac70f3873ab08da8ebc7d336b5d37
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2025-07-14 09:53:29 +0530

    New file generate

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import HerroBanner from "./component/spardhalead/HerroBanner";
-import HomePage from "./component/spardhalead/HomePage";
+import HomePage from "./component/spardhaleaduk/HomePage";
 
 export default function Home() {
   return (
